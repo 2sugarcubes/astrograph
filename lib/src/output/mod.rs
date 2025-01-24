f9fdfa4473f041324c4861230a43c40e993afb43
Lucy M <105619409+2sugarcubes@users.noreply.github.com>
2025-01-24 11:30:52 -0700

    Changed output trait signiture

--- lib/src/output/mod.rs
+++ lib/src/output/mod.rs
@@ -24,59 +24,32 @@ pub trait Output: DynClone + Debug {
     /// implementations may panic if there is an error in the filesystem e.g. writing is not
     /// allowed for a user in a specific directory, or one or more of the directories are files
     /// that have already been created
-    fn write_observations_to_file(
+    fn write_observations(
         &self,
         observations: &[(Arc, Spherical<Float>)],
-        path: &Path,
+        observatory_name: &str,
+        time: i128,
+        output_path_root: &Path,
     ) -> Result<(), std::io::Error>;
+
+    fn flush(&self) -> Result<(), std::io::Error> {
+        Ok(())
+    }
 }
 dyn_clone::clone_trait_object!(Output);
 
-/// Changes the extension of the given path, even when the last element is perceived to be a directory
-fn set_extension(path: &Path, extension: &str) -> PathBuf {
-    let mut path = path.to_path_buf();
-    // Make sure Rust isn't trying to be clever and take our file without an extension and convert
-    // it to a directory
-    if path.is_dir() {
-        path.pop();
-    }
-    path.set_extension(extension);
-    return path;
+pub fn to_default_path(
+    output_path_root: &Path,
+    observatory_name: &str,
+    time: i128,
+    extension: &str,
+) -> PathBuf {
+    let mut path = output_path_root.to_owned();
+    path.push(observatory_name);
+    path.push(format!("{time:010}{extension}"));
+
+    path
 }
 
 #[cfg(test)]
-mod tests {
-    use std::path::PathBuf;
-
-    use crate::output::set_extension;
-
-    #[test]
-    fn set_extension_for_file_without_extension() {
-        let path = set_extension(&PathBuf::from("path/to/file"), "txt");
-        assert!(
-            path.eq(&PathBuf::from("path/to/file.txt")),
-            "Path {} does not match path 'path/to/file.txt'",
-            path.to_str().unwrap_or("[COULD NOT DISPLAY]")
-        );
-    }
-
-    #[test]
-    fn set_extension_for_file_with_extension() {
-        let path = set_extension(&PathBuf::from("path/to/file.bin"), "txt");
-        assert!(
-            path.eq(&PathBuf::from("path/to/file.txt")),
-            "Path {} does not match path 'path/to/file.txt'",
-            path.to_str().unwrap_or("[COULD NOT DISPLAY]")
-        );
-    }
-
-    #[test]
-    fn set_extension_for_directory() {
-        let path = set_extension(&PathBuf::from("path/to/directory/".to_string()), "txt");
-        assert!(
-            path.eq(&PathBuf::from("path/to/directory.txt")),
-            "Path {} does not match path 'path/to/directory.txt'",
-            path.to_str().unwrap_or("[COULD NOT DISPLAY]")
-        );
-    }
-}
+mod tests {}
