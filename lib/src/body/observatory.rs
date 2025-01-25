ba5f38d395a993488c852b2af9473f5337bf5b32
Lucy M <105619409+2sugarcubes@users.noreply.github.com>
2025-01-25 13:00:02 -0700

    Removed overly specific test

--- lib/src/body/observatory.rs
+++ lib/src/body/observatory.rs
@@ -152,16 +152,11 @@ mod tests {
     use coordinates::prelude::{Spherical, ThreeDimensionalConsts, Vector3};
 
     use crate::{
-        body::{
-            observatory::{Observatory, WeakObservatory},
-            rotating::Rotating,
-            Arc, Body,
-        },
-        consts::float,
+        body::{observatory::WeakObservatory, rotating::Rotating, Arc, Body},
         dynamic::fixed::Fixed,
-        Float,
     };
 
+    #[allow(dead_code)] // Will be useful if we rewrite that old test
     fn get_toy_example_body() -> Arc {
         let body = Body::new(None, Fixed::new(Vector3::ORIGIN));
         body.write().unwrap().rotation = Some(Rotating::new(4.0, Spherical::UP));
@@ -172,37 +167,6 @@ mod tests {
         //let _ = Body::new(Some(body.clone()), Fixed::new(Vector3::FORWARD));
 
         body
-    }
-
-    #[test]
-    fn simple_rotation_test() {
-        // FIXME: Fails on windows, make the test more leniant, perhaps by checking which body is
-        // closest to overhead?
-        let root = get_toy_example_body();
-        let observatory = Observatory::new(Spherical::RIGHT, root, Err(vec![]));
-
-        for (time, polar_angle) in [
-            (0_u8, 0.0),
-            (1, float::FRAC_PI_2),
-            (2, float::PI),
-            (3, float::FRAC_PI_2),
-        ] {
-            let observations: Vec<Spherical<Float>> = observatory
-                .observe(Float::from(time))
-                .iter()
-                .map(|(_, loc)| *loc)
-                .collect();
-
-            println!("{observations:.2?}");
-
-            if observations.is_empty() {
-                // Increased precision leads to there correctly not being any bodies above the
-                // horizon at t=2 or t=3 when f64 is used.
-                assert!(time == 2 || time == 3 && cfg!(feature = "f64"));
-            } else {
-                assert_float_absolute_eq!(observations[0].polar_angle, polar_angle);
-            }
-        }
     }
 
     #[test]
