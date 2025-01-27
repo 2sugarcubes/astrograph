8fb8b766d50721a878a1429e65b84a927719e02f
Lucy M <105619409+2sugarcubes@users.noreply.github.com>
2025-01-27 15:54:22 -0700

    Made benches smaller for faster itteration

--- lib/benches/program.rs
+++ lib/benches/program.rs
@@ -1,17 +1,47 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use std::hint::black_box;
+use astrolabe::{
+    generator::{artifexian::ArtifexianBuilder, Generator},
+    program::Program,
+};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
+#[derive(Clone, Debug, Default)]
+struct Output;
+
+impl astrolabe::output::Output for Output {
+    fn write_observations(
+        &self,
+        _observations: &[astrolabe::LocalObservation],
+        _observatory_name: &str,
+        _time: i128,
+        _output_path_root: &std::path::Path,
+    ) -> Result<(), std::io::Error> {
+        black_box(Ok(()))
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+fn observations(c: &mut Criterion) {
+    let mut program: Program =
+        serde_json::from_str(include_str!("../../assets/solar-system.program.json")).unwrap();
+
+    program.add_output(Box::new(Output));
+
+    // Bench observations
+    c.bench_function("observe 1,000", |b| {
+        b.iter(|| program.make_observations(black_box(0), 1_000, None))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn generation(c: &mut Criterion) {
+    // Bench universe generation
+    let generator = ArtifexianBuilder::default()
+        .star_count(100_000)
+        .build()
+        .unwrap();
+    c.bench_function("gen 100,000", |b| {
+        b.iter(|| generator.generate(&mut thread_rng()))
+    });
+}
+
+criterion_group!(benches, generation, observations);
 criterion_main!(benches);
