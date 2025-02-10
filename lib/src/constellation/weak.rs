b00c83d97ddae4e7e7d3ee4b5fa2e2e41f2cb842
Lucy Magnusson <105619409+2sugarcubes@users.noreply.github.com>
2025-02-09 18:18:53 -0700

    fix(constellation): allow constellations to be empty

--- lib/src/constellation/weak.rs
+++ lib/src/constellation/weak.rs
@@ -5,6 +5,7 @@ use log::{trace, warn};
 #[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub struct Weak {
     /// List of the edges and the IDs of the bodies that mark their ends
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     edges: Vec<(Vec<usize>, Vec<usize>)>,
 }
 
